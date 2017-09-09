cede66fe5689e4967d63ed462569e10d9eb4924b
vlad9508 <vladimir_xb_x3@hotmail.com>
2017-09-09 12:53:37 -0500

    mandar datos a la base de datos

--- modulo.php
+++ modulo.php
@@ -14,8 +14,11 @@
 	$conce4 = $_POST['conce4'];
 	$conce5 = $_POST['conce5'];
 
-	//$con=mysqli_connect("localhost", "root", "") or die("error al establecer la conexion");
-	//$db=mysqli_select_db("factura", $con);
+	$con=mysqli_connect("localhost", "root", "", "factura") or die("error al establecer la conexion");
+	//$db=mysqli_select_db("facturas", $con);
+
+	mysqli_query($con, "INSERT INTO facturas VALUES('', '". $fecha ."', '".$empresa."', '".$cliente."', ".$total.", ".$sub.", ".$iva.", '".$tri."', '".$tri2."', '".$tri3."', '".$conce."', '".$conce2."', '".$conce3."', '".$conce4."', '".$conce5."'); ");
+	mysql_close($con);
 
 	$file = fopen("factura_". $cliente . ".ini", "a");
 	fwrite($file, "[Factura 1] \r\n fecha: ". $fecha . "\r\n empresa: ". $empresa . "\r\n cliente: ". $cliente . "\r\n total: ". $total . "\r\n sub total: ". $sub . "\r\n iva: ". $iva. "\r\n triple: ". $tri . "\r\n concepto: ". $conce . "\r\n");
