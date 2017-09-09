ff686ae15fbcafabb4b1443a5f1a51527dc44e85
vlad9508 <vladimir_xb_x3@hotmail.com>
2017-09-08 23:10:02 -0500

    formulari
    parte del formulario y pruebas

---
+++ modilo_factura.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>.:Desika:.</title>
+</head>
+<body>
+	<div>
+		<form>
+			<p>Fecha: </p>
+			<input type="date" name="fecha" min="2012-01-01">
+			<br>
+			<p>Empresa: </p>
+			<input type="text" name="empresa">
+			<br>
+			<p>Cliente: </p>
+			<input type="text" name="cliente">
+			<br>
+			<p>Total</p>
+			<input type="text" name="total">
+			<br>
+			<p>Sub Total</p>
+			<input type="text" name="sub">
+			<br>
+			<p>I.V.A</p>
+			<input type="text" name="iva">
+			<br>
+			<p>triple 1</p>
+			<input type="text" name="tri">
+			<br>
+			<p>triple 2</p>
+			<input type="text" name="tri2">
+			<br>
+			<p>triple 3</p>
+			<input type="text" name="tri3">
+			<br>
+			<p>Concepto</p>
+			<textarea rows="10" cols="40" id="conce"></textarea>
+			<br>
+			<p>Concepto 2</p>
+			<textarea rows="10" cols="40" id="conce2"></textarea>
+			<br>
+			<p>Concepto 3</p>
+			<textarea rows="10" cols="40" id="conce3"></textarea>
+			<br>
+			<p>Concepto 4</p>
+			<textarea rows="10" cols="40" id="conce4"></textarea>
+			<br>
+			<p>Concepto 5</p>
+			<textarea rows="10" cols="40" id="conce5"></textarea>
+			<br>
+		</form>
+	</div>
+</body>
+</html>
